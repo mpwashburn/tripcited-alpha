409f59c1e7c7ca1c651435123b1dae70e78e74ab
Michael Washburn <washburn.michael@gmail.com>
2018-03-20 21:17:46 -0700

    commiting changes forgotten last night

--- models/Trip.js
+++ models/Trip.js
@@ -3,6 +3,29 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema ({
 
+  trip:{
+    createdDate: date(),
+    title:{
+      image:String,
+      video:String,
+      name:String,
+      date:String,
+    },
+    place:{[
+      {
+        type:String
+        
+      }
+    ]}
+    companions:{[]},// A reference to other prolies and some of their details
+    role{
+      admin:{[]},
+      viewer:{[]}, // Maybe just can comment, chat, vote, etc.. within main itinerary
+    },
+    locations:{[]},
+
+  }
+
 });
 
 // Create collection and add Schema
